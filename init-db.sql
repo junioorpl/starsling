dc18c50c59b416615b6cc96aab9e4cdafc7503f1
Euripedes Cabral <euripedescabral@192.168.1.5>
2025-09-16 02:43:16 -0300

    feat: add PostgreSQL initialization script

---
+++ init-db.sql
@@ -0,0 +1,14 @@
+-- Initialize the database
+CREATE DATABASE IF NOT EXISTS starsling;
+
+-- Create the starsling user if it doesn't exist
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'starsling') THEN
+        CREATE ROLE starsling WITH LOGIN PASSWORD 'starsling_password';
+    END IF;
+END
+$$;
+
+-- Grant privileges
+GRANT ALL PRIVILEGES ON DATABASE starsling TO starsling;
